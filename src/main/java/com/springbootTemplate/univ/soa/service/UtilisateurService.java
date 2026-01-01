0b8055ec7f84f0e49be28509eddb028c238ca78b
Sabine22-alt <nacerazemriz@gmail.com>
2026-01-01 12:43:03 +0100

    Ajout d'utilisateur controller, utilisateurService, UtilisateurServiceImpl

--- src/main/java/com/springbootTemplate/univ/soa/service/UtilisateurService.java
+++ src/main/java/com/springbootTemplate/univ/soa/service/UtilisateurService.java
@@ -4,6 +4,8 @@ import com.springbootTemplate.univ.soa.dto.*;
 
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+
 public interface UtilisateurService {
 
     UtilisateurResponseDto register(UtilisateurCreateDto createDto);
@@ -19,4 +21,19 @@ public interface UtilisateurService {
     UtilisateurResponseDto updateUtilisateur(Long id, UtilisateurUpdateDto updateDto);
 
     void deleteUtilisateur(Long id);
+
+    // Added methods for connected user
+    UtilisateurResponseDto getUtilisateurConnecte(Authentication authentication);
+
+    UtilisateurResponseDto updateUtilisateurConnecte(Authentication authentication, UtilisateurUpdateDto updateDto);
+
+    void deleteUtilisateurConnecte(Authentication authentication);
+
+    // Preferences
+    PreferencesDto getPreferences(Authentication authentication);
+
+    PreferencesDto updatePreferences(Authentication authentication, PreferencesDto preferencesDto);
+
+    // Export
+    byte[] exportUserData(Authentication authentication);
 }
